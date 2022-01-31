b87518af79b9799f2b506413f87ced3656ca2dba
Ivan <ivan.d.andrea1@gmail.com>
2022-01-31 11:31:06 +0100

    redis fixing

--- server/src/constants/const.ts
+++ server/src/constants/const.ts
@@ -4,6 +4,7 @@ export const PORT  = process.env.PORT || 4000;
 export const __prod__ = process.env.NODE_ENV === 'production';
 export const SECRET = process.env.SECRET;
 export const REDIS_TLS_URL = process.env.REDIS_TLS_URL;
+export const REDIS_URL=process.env.REDIS_URL;
 export const OneDay= 1000* 60*60*24;
 export const COOKIENAME = 'qid';
 export const ALLOW_ORIGIN= process.env.ALLOWED_ORIGIN;
--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS{
         ATLAS_CONNETCION:string;
         SECRET:string ;
         REDIS_TLS_URL:string| undefined;
+        REDIS_URL:string |undefined;
         ALLOWED_ORIGIN:string;
         SMTP_USER:string;
         SMTP_PASS:string;
