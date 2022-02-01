e83c76ff2d49813d02d1f01e3b0342153b8b8c31
Ivan <ivan.d.andrea1@gmail.com>
2022-02-01 17:34:08 +0100

    theming

---
+++ client/src/Components/Dashboard/Dashboard.style.tsx
@@ -0,0 +1,18 @@
+export const flexColumCenter={
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",  
+}
+export const flexRowCenter={
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  }
+  export const title={
+    
+        m: 25,
+        textAlign: "center",
+      
+  }
---
+++ client/src/Themes/darkTheme.tsx
@@ -0,0 +1,147 @@
+
+import {ThemeOptions}from "@mui/material"
+// DARK THEME
+
+export const themeOptionsDark: ThemeOptions = {
+    palette: {
+      primary: {
+        main: '#f7f1e3',
+      },
+      secondary: {
+        main: '#090910',
+      },
+      background: {
+        default: "#090910"
+      },
+      text:{
+        primary:"#f7f1e3",
+        secondary:"#f7f1e3"
+      }
+    },
+    components:{MuiCardContent:{
+      defaultProps:{
+        sx:{
+          color:"#090910",
+          backgroundColor:"#f7f1e3",
+        }
+      }
+    },
+    MuiCheckbox:{
+        defaultProps:{
+            color:"secondary"
+        } 
+    },
+    MuiCard:{
+      defaultProps:{
+        sx:{
+          m:5,
+          boxShadow:0,
+          color:"#090910",
+          backgroundColor:"#f7f1e3",
+            }
+      }
+    },
+    MuiPaper:{
+      defaultProps:{
+        sx:{
+            backgroundColor:"#f7f1e3",
+            color:"#090910", 
+            boxShadow:0,
+            }
+      }
+    },
+    MuiCardActions:{
+      defaultProps:{
+        sx:{
+          color:"#090910",
+          backgroundColor:"#f7f1e3",
+       
+         
+        }
+      }
+    }
+    ,MuiSelect:{
+      defaultProps:{
+  
+        sx:{
+          borderRadius:"10px",
+          background: "#090910",
+          opacity:0.9,
+       
+         }
+      }
+    },
+    MuiTextField:{
+      defaultProps:{
+  
+        sx:{
+          borderRadius:"10px",
+          background: "#090910",
+          opacity:0.9,
+         
+         }
+      }
+    },MuiSlider:{
+      styleOverrides:{
+        markLabel:{
+        color:"#090910"
+        },
+        rail:{
+          color:"#090910"
+        },
+        track:{color:"red"},
+        thumbColorPrimary:{
+          color:"#090910"
+        }
+        
+      }
+    },
+    MuiButton:{
+      styleOverrides:
+      { outlined:{
+        color:"primary"
+        
+      },
+      text:
+        {color:"inherit",
+          position:"relative",
+          display:"inline-block",
+         
+           "&:after" :{    
+             background: "none repeat scroll 0 0 transparent",
+             backgroundColor:"#090910",
+             bottom: 0,
+             content: '""',
+             display: "block",
+             height: "2px",
+             left: "50%",
+             position: "absolute",
+             transition: "width 0.3s ease 0s, left 0.3s ease 0s",
+             width: 0,
+           },
+           "&:hover:after" :{ 
+             width: "100%", 
+             left: 0, 
+           },}
+         },
+      defaultProps:{
+        sx:{
+          margin:2,
+        }
+      }
+    },
+    MuiMenuItem:{
+      defaultProps:{
+        sx:{
+          backgroundColor:"primary",
+          color:"black"
+        }
+      }
+    }},
+    shape: {
+      borderRadius: 5,
+      
+    },
+    spacing: 1,
+    
+  }
