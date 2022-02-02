ff5222b93d9703b27e3395cc607d30233381c71a
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2022-02-02 09:33:26 +0100

    menu mobile fixed

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,26 +1,23 @@
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
 
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import logo from "../../Assets/Images/logo.svg";
+import MenuIcon from '@mui/icons-material/Menu';
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
+import { useMutation, gql } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
 
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import logo from '../../Assets/Images/logo.svg'
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
 
-import { useMutation,gql } from '@apollo/client';
-import { useNavigate } from 'react-router-dom';
-
-import ThemeSwitch from '../ThemeSwitch/ThemeSwitch';
-
-
-import { Button, Grid } from '@mui/material';
+import { Button, Grid } from "@mui/material";
 
 const LOGOUT_MUT = gql`
   mutation {
@@ -28,37 +25,26 @@ const LOGOUT_MUT = gql`
   }
 `;
 
-
-
-
 export default function Header() {
-
-    const navigate = useNavigate();
-  
+  const navigate = useNavigate();
   const [logout] = useMutation(LOGOUT_MUT);
 
   const logoutEvent = async (event: React.SyntheticEvent) => {
     event.preventDefault();
-
     try {
       await logout();
     } catch (err) {
       console.error(err);
     }
-
     navigate("/");
     window.location.reload();
   };
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-    React.useState<null | HTMLElement>(null);
+   useState<null | HTMLElement>(null);
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
 
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
@@ -73,19 +59,19 @@ export default function Header() {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
@@ -95,19 +81,19 @@ export default function Header() {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
@@ -132,96 +118,86 @@ export default function Header() {
         </IconButton>
         <p>Notifications</p>
       </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
+      <MenuItem sx={{display:"flex", alignItems:"center",justifyContent:"center"}}>
+    <ThemeSwitch/>
+    </MenuItem>  
     </Menu>
   );
-
-
-  
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-           
-      <IconButton aria-label="" href="/">
-             <img
-             src={logo}
-             alt="logo"
-             style={{
-               flex: 1,
-               alignSelf: "stretch",
-               width: 85,
-               height: 45,
-               marginTop: 5,
-               }}
+          <IconButton aria-label="" href="/">
+            <img
+              src={logo}
+              alt="logo"
+              style={{
+                flex: 1,
+                alignSelf: "stretch",
+                width: 85,
+                height: 45,
+                marginTop: 5,
+              }}
             />
-      </IconButton>
-        <Grid container spacing={0} sx={{display:"flex", justifyContent:"space-between",flexWrap:"nowrap"}}>
-          
-        
-          
-         <Grid item sx={{alignSelf:"center"}}>
-          <Button variant="text" color="inherit" href="/profile"  >
-           Profile
-          </Button>
-          <Button variant="text" color="inherit" onClick={logoutEvent}>
-            Logout
-          </Button>
-          </Grid>
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-          <Grid item>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-              >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-              >       
-            </IconButton>
-            <ThemeSwitch /> 
+          </IconButton>
+          <Grid
+            container
+            spacing={0}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              flexWrap: "nowrap",
+            }}
+          >
+            <Grid item sx={{ alignSelf: "center" }}>
+              <Button variant="text" color="inherit" href="/profile">
+                Profile
+              </Button>
+              <Button variant="text" color="inherit" onClick={logoutEvent}>
+                Logout
+              </Button>
             </Grid>
-          </Box>
-          
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-              >
-            </IconButton>
-          </Box>
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <Grid item>
+                <IconButton
+                  size="large"
+                  aria-label="show 4 new mails"
+                  color="inherit"
+                >
+                  <Badge badgeContent={4} color="error">
+                    <MailIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton
+                  size="large"
+                  aria-label="show 17 new notifications"
+                  color="inherit"
+                >
+                  <Badge badgeContent={17} color="error">
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton>
+              
+                <ThemeSwitch />
               </Grid>
+            </Box>
+
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+               <Button variant="text" >
+               <MenuIcon/>
+               </Button> 
+              </IconButton>
+            </Box>
+          </Grid>
         </Toolbar>
       </AppBar>
       {renderMobileMenu}
@@ -229,6 +205,3 @@ export default function Header() {
     </Box>
   );
 }
-
-
-
