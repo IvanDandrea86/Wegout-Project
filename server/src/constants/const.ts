4f200bb5c3602ca7717487ac07bc2ada6391412f
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2022-02-07 16:57:10 +0100

    staging test

--- server/src/constants/const.ts
+++ server/src/constants/const.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config()
 export const PORT  = process.env.PORT || 4000;
-export const __prod__ = process.env.NODE_ENV === 'production';
+export const __prod__ = process.env.NODE_ENV === 'production'||'staging';
 export const SECRET = process.env.SECRET;
 export const REDIS_TLS_URL = process.env.REDIS_TLS_URL;
 export const REDIS_URL=process.env.REDIS_URL;
