44c4740d0afe89f9dc09b1b2e2614c8127aea21a
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2022-02-08 12:56:07 +0100

    supub redis works

--- client/src/Utils/createApolloClient.tsx
+++ client/src/Utils/createApolloClient.tsx
@@ -5,26 +5,26 @@ import {
   import { split, HttpLink } from "@apollo/client";
 import { WebSocketLink } from "@apollo/client/link/ws";
 import { getMainDefinition } from "@apollo/client/utilities";
-var linkUri=""
-if (process.env.NODE_ENV === "development") {
-  linkUri="/subscription"
-} 
-else{
-  linkUri="ws://localhost:4000/subscriptions"
-}
+
 const httpLink = new HttpLink({
   uri: "http://localhost:4000/graphql", 
   credentials: "include",
 });
+var linkUri=""
+if (process.env.NODE_ENV === "production") {
+  linkUri="/subscription"
+  httpLink.options.uri="/graphql";
+} 
+else{
+  linkUri="ws://localhost:4000/subscriptions"
+}
 const wsLink = new WebSocketLink({
   uri: `${linkUri}`,
   options: {
     reconnect: true,
   },
 });
-if (process.env.NODE_ENV === "development"){
-  httpLink.options.uri="/graphql";
-}
+
 const link = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
