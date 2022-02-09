eff277863dde4c956eb4ca038f58fe33d617af74
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2022-02-09 16:53:56 +0100

    deploy to heroku

--- client/src/Components/Buttons/ChatButton.tsx
+++ client/src/Components/Buttons/ChatButton.tsx
@@ -38,11 +38,11 @@ export const ChatButton = (props:any) => {
     if (errors){return <ErrorMess/>}
 
     if (data.createChat.errors ===null){
-        await  channel.setChatChannel(data.createChat.chat._id)
+         channel.setChatChannel(data.createChat.chat._id)
         navigate("../../chat")   
     }
     else{
-      await  channel.setChatChannel(data.createChat.chat._id)
+        channel.setChatChannel(data.createChat.chat._id)
          navigate("../../chat")
         // window.location.reload();
     }}
